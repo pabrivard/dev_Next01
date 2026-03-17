b79a26dc4df96949e3b04fbada024da414d138cb
pabrivard <pierre.abrivard@outlook.com>
2026-03-17 15:56:53 +0100

    fix: use hardcoded production URL in capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,13 +1,17 @@
-import { CapacitorConfig } from "@capacitor/cli";
+import { CapacitorConfig } from '@capacitor/cli'
+
+const isProd = process.env.NODE_ENV === 'production'
 
 const config: CapacitorConfig = {
-  appId: "com.dev.next01",
-  appName: "Dev_Next01",
-  webDir: ".next",
-  server: {
-    url: process.env.NEXT_PUBLIC_APP_URL,
-    cleartext: false,
-  },
-};
+  appId: 'com.dev.next01',
+  appName: 'Dev_Next01',
+  webDir: '.next',
+  server: isProd
+    ? {
+        url: 'https://your-production-url.com',
+        cleartext: false,
+      }
+    : undefined,
+}
 
-export default config;
+export default config
